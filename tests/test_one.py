ef0ec896bdc62b5f3abf8451374f953edac7ed11
Roberto Polli <roberto.polli@par-tec.it>
2016-04-11 17:07:43 +0200

    feat: only py2 tests succeed

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,4 +1,8 @@
 from nose.tools import * 
 def test_sample():
-    for i in range(5):
+    for i in range(1,6):
         yield assert_true, i
+
+def test_python():
+    from sys import version_info
+    assert version_info < (3,0), "Python 3 is not supported."
