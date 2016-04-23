d9ed4bbafc388d5986eea35a461e4b4d4c9db8f1
Roberto Polli <roberto.polli@par-tec.it>
2016-04-23 15:42:15 +0200

    fix: workaround openshift uids

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+function generate_passwd_file() {
+  export USER_ID=$(id -u)
+  export GROUP_ID=$(id -g)
+  grep -v ^python /etc/passwd > "$HOME/passwd"
+  echo "python:x:${USER_ID}:${GROUP_ID}:Python Server:${HOME}:/bin/bash" >> /etc/passwd
+}
+
+
+python app.py
